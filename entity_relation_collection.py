e694a66686fa8870a0c67dc8d9f4afd1499dd34b
roy <rtcohen99@gmail.com>
2021-07-26 21:45:10 +0200

    Output agent's state destrictipns and available actions and clean output for further processing using OpenIE.

--- entity_relation_collection.py
+++ entity_relation_collection.py
@@ -1,6 +1,7 @@
 import argparse
 import textworld
 import numpy as np
+import re
 
 class RandomAgent(textworld.Agent):
     """ A random agent that selects randomly chosen admissable action. """
@@ -16,37 +17,38 @@ class RandomAgent(textworld.Agent):
     def act(self, state, reward, done):
         return self.rng.choice(state.admissible_commands)
 
-class WalkthroughDone(NameError):
-    pass
+# class WalkthroughDone(NameError):
+#     pass
 
-class WalkthroughAgent(textworld.Agent):
-    """ Agent that follows a given list of commands"""
+# class WalkthroughAgent(textworld.Agent):
+#     """ Agent that follows a given list of commands"""
 
-    def __init__(self, commands=None):
-        self.commands = commands
+#     def __init__(self, commands=None):
+#         self.commands = commands
     
-    def reset(self, env):
-        env.activate_state_tracking()
-        env.display_command_during_render = True
+#     def reset(self, env):
+#         env.activate_state_tracking()
+#         env.display_command_during_render = True
         
-        if self.commands is not None:
-            self._commands = iter(self.commands)
-            return  # Commands already specified.
+#         if self.commands is not None:
+#             self._commands = iter(self.commands)
+#             return  # Commands already specified.
         
-        if not hasattr(env, "game"):
-            raise NameError("WalkthroughAgent is only supported for generated games")
+#         if not hasattr(env, "game"):
+#             raise NameError("WalkthroughAgent is only supported for generated games")
 
-        self._commands = iter(env.game.quests[0].commands)
+#         print(env.game.quests)
+        # self._commands = iter(env.game.quests[0].commands)
 
     
-    def act(self, state, reward, done):
-        try:
-            action = next(self._commands)
-        except StopIteration:
-            raise WalkthroughDone()
+    # def act(self, state, reward, done):
+    #     try:
+    #         action = next(self._commands)
+    #     except StopIteration:
+    #         raise WalkthroughDone()
 
-        action = action.strip()
-        return action
+    #     action = action.strip()
+    #     return action
         
 
 
@@ -66,6 +68,7 @@ def run_agent(agent, game, output_file, steps,epochs):
         for step in range (steps):
             command = agent.act(state, reward, done)
 
+            
             output_file.write(state.description)
             output_file.write("Actions: " + str(state.admissible_commands) + '\n')
             actions.update(state.admissible_commands)
@@ -88,3 +91,26 @@ def get_entity_relation(game_info):
     actions.update(run_agent(agent=RandomAgent(),game=game,output_file=output_file,steps=1000,epochs=5))
 
     output_file.close()
+
+
+    output_file = open('./cleaned_temp.txt', 'w')
+    with open('./temp.txt', 'r') as f:
+        cur = []
+        for line in f:
+            # print(line)
+            if line != '---------' and "Actions:" not in str(line) and "Taken action:" not in str(
+                    line):
+                cur.append(line)
+            else:
+                cur = [a.strip() for a in cur]
+                cur = ' '.join(cur).strip().replace('\n', '').replace('---------', '')
+                cur = re.sub("(?<=-\=).*?(?=\=-)", '', cur)
+                cur = cur.replace("-==-", '').strip()
+                cur = '. '.join([a.strip() for a in cur.split('.')])
+                output_file.write(cur + '\n')
+                cur = []
+
+    output_file.close()
+
+    input_file = open('./cleaned_temp.txt', 'r')
+
