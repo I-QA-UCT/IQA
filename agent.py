Fix dimention issue on qa loss calculation.

agent.py
@@ -780,17 +780,27 @@ class Agent:
         transitions = self.qa_replay_memory.sample(self.replay_batch_size)
         batch = qa_memory.qa_Transition(*zip(*transitions))
 
+        adj_mat_list = batch.adj_mat
+        temp1 = []
+        for adj in adj_mat_list:
+            temp1.append(len(adj.x))
+        
+        loader = DataLoader(adj_mat_list, batch_size=self.replay_batch_size)
+        batch_gat = next(iter(loader))
+        gat_out = self.GAT(batch_gat, temp1)
+
         observation_list = batch.observation_list
         quest_list = batch.quest_list
         answer_strings = batch.answer_strings
         answer_position = np.array(_words_to_ids(answer_strings, self.word2id))
         groundtruth = to_pt(answer_position, self.use_cuda)  # batch
 
+
         input_quest, input_quest_char, _ = self.get_agent_inputs(quest_list)
         
         input_observation, input_observation_char, observation_id_list =  self.get_agent_inputs(observation_list)
 
-        answer_distribution, _, _ = self.answer_question(input_observation, input_observation_char, observation_id_list, input_quest, input_quest_char, use_model="online")  # batch x vocab
+        answer_distribution, _, _ = self.answer_question(input_observation, input_observation_char, observation_id_list, input_quest, input_quest_char, gat_out, use_model="online")  # batch x vocab
 
         batch_loss = NegativeLogLoss(answer_distribution, groundtruth)  # batch
         return torch.mean(batch_loss)

layers.py
@@ -463,7 +463,7 @@ class AnswerPointer(torch.nn.Module):
             self.w_2 = torch.nn.Linear(block_hidden_dim, 1)
 
     def forward(self, M1, M2, mask, GAT):
-        X_concat = torch.cat([M1, M2, torch.cat([GAT] * len(M1[0])).unsqueeze(0)], dim=-1)
+        X_concat = torch.cat([M1, M2, torch.stack([GAT] * len(M1[0]),dim=1)], dim=-1)
         X = torch.relu(self.w_1(X_concat))
 
         X_advantage = torch.relu(self.w_1_advantage(X_concat))

train.py
@@ -320,9 +320,9 @@ def train(data_path):
         # push qa experience into qa replay buffer
         for b in range(batch_size):  # data points in batch
             # if the agent is not in the correct state, do not push it into replay buffer
-            if np.sum(sufficient_info_reward_np[b]) == 0.0:
-                continue
-            agent.qa_replay_memory.push(False, qa_reward_np[b], answerer_input[b], questions[b], answers[b])
+            # if np.sum(sufficient_info_reward_np[b]) == 0.0:
+            #     continue
+            agent.qa_replay_memory.push(False, qa_reward_np[b], adj_mat, answerer_input[b], questions[b], answers[b])
 
         # assign sufficient info reward and counting reward to the corresponding steps
         counting_rewards_np = np.stack(counting_rewards_np, 1)  # batch x game step